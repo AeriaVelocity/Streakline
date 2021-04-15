2359722cb134d9a520dafdbe11d068988455a7f8
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-04-15 14:19:40 +0100

    Add ~del command

--- ed.py
+++ ed.py
@@ -33,7 +33,16 @@ try:
 		elif line == "~help":
 			print("~save - Save the file and exit.")
 			print("~exit - Exit without saving.")
-			print("~show - Show the file contents, and what will be saved when the program quits.")
+			print("~show - Show the contents of the file and buffer.")
+			print("~del - Remove the last line from the file buffer.")
+		elif line == "~exit":
+			pass
+		elif line == "~del":
+			if len(filebuffer) < 1:
+				print("Nothing to delete.")
+			else:
+				print("Previous line deleted.")
+				filebuffer.pop()
 		elif "~" in line:
 			print(line + " is an unknown command.")
 		if not "~" in line:
@@ -45,5 +54,9 @@ if line == "~save":
 	print("File '" + filename + "' - " + str(filesize) + " bytes saved.")
 elif line == "~exit":
 	print("File '" + filename + "' - closed without saving.")
+	if filesize < 1:
+		os.remove(filename)
 else:
 	print("File '" + filename + "' - closed without saving.")
+	if filesize < 1:
+		os.remove(filename)
