f2e3ae9ed0eae0013f2b04c51a62f5bea5bf2250
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-04-15 13:51:09 +0100

    #save now exits

--- ed.py
+++ ed.py
@@ -2,8 +2,12 @@ import os
 filename = ""
 line = ""
 filebuffer = []
-while filename == "":
-    filename = input("What file name? > ")
+try:
+	while filename == "":
+		filename = input("What file name? > ")
+except KeyboardInterrupt:
+	print("\nQuitting.")
+	exit(0)
 if os.path.exists(filename):
     print("Warning - this file already exists.")
 openedfile = open(filename, "a");
@@ -11,15 +15,12 @@ filesize = os.path.getsize(filename)
 print("File '" + filename + "' opened for writing.")
 print("Start typing. Changes will be saved in buffer.")
 try:
-	while line != "#exit":
+	while line != "#exit" and line != "#save":
 		line = input("ed > ")
 		if line == "#save":
 			for x in range(len(filebuffer)):
 				openedfile.write(filebuffer[x] + "\n")
-			print("File '" + filename + "' - " + str(filesize) + " bytes saved.")
-			print("Type #exit to stop editing.")
-			filebuffer = []
-		if line == "#show":
+		elif line == "#show":
 			filer = open(filename, 'r')
 			if filesize > 0:
 				print("Previously stored:")
@@ -29,13 +30,20 @@ try:
 				print("To be saved:")
 				for x in range(len(filebuffer)):
 					print(filebuffer[x])
-		if line == "#help":
-			print("#save - Save the file.")
-			print("#exit - Stop editing. This will not save your changes - use #save, then #exit.")
+		elif line == "#help":
+			print("#save - Save the file and exit.")
+			print("#exit - Exit without saving.")
 			print("#show - Show the file contents, and what will be saved when the program quits.")
+		elif "#" in line:
+			print(line + " is an unknown command.")
 		if not "#" in line:
 			filebuffer.append(line)
 except KeyboardInterrupt:
 	print()
 openedfile.close()
-print("File '" + filename + "' closed.")
+if line == "#save":
+	print("File '" + filename + "' - " + str(filesize) + " bytes saved.")
+elif line == "#exit":
+	print("File '" + filename + "' - closed without saving.")
+else:
+	print("File '" + filename + "' - closed without saving.")
