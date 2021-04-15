31062b1532436e9b74c8caf493c4ed2a06de4ab8
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-04-15 13:36:47 +0100

    Add files via upload

--- ed.py
+++ ed.py
@@ -4,27 +4,34 @@ line = ""
 filebuffer = []
 while filename == "":
     filename = input("What file name? > ")
-filesize = os.path.getsize(filename)
 if os.path.exists(filename):
     print("Warning - this file already exists.")
 openedfile = open(filename, "a");
-print("Start typing. It'll automatically be saved. Type '#save' to exit.")
-while line != "#save":
-    line = input("ed > ")
-    if line == "#save":
-        pass
-    if line == "#show":
-        filer = open(filename, 'r')
-        if filesize > 0:
-            print("Previously stored:")
-            print(filer.read(), end='')
-        filer.close()
-        if len(filebuffer):
-            print("To be saved:")
-            for x in range(len(filebuffer)):
-                print(filebuffer[x])
-    if not "#" in line:
-        openedfile.write(line + "\n")
-        filebuffer.append(line)
+filesize = os.path.getsize(filename)
+print("File '" + filename + "' opened for writing.")
+print("Start typing. Changes will be saved in buffer.")
+try:
+	while line != "#exit":
+		line = input("ed > ")
+		if line == "#save":
+			for x in range(len(filebuffer)):
+				openedfile.write(filebuffer[x] + "\n")
+			print("File '" + filename + "' - " + str(filesize) + " bytes saved.")
+			print("Type #exit to stop editing.")
+			filebuffer = []
+		if line == "#show":
+			filer = open(filename, 'r')
+			if filesize > 0:
+				print("Previously stored:")
+				print(filer.read(), end='')
+			filer.close()
+			if len(filebuffer):
+				print("To be saved:")
+				for x in range(len(filebuffer)):
+					print(filebuffer[x])
+		if not "#" in line:
+			filebuffer.append(line)
+except KeyboardInterrupt:
+	print()
 openedfile.close()
-print("File '" + filename + "' - " + str(filesize) + " bytes saved.")
+print("File '" + filename + "' closed.")
