7c997481ac58b78a803f681a0873a3fe200a36e3
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-04-15 13:10:26 +0100

    Add files via upload

--- ed.py
+++ ed.py
@@ -1,14 +1,30 @@
 import os
 filename = ""
 line = ""
+filebuffer = []
 while filename == "":
     filename = input("What file name? > ")
-openedfile = open(filename, "a");
-print("Start typing. It'll automatically be saved. Type '#~#' to exit.")
-while line != "#~#":
-    line = input("ed > ")
-    if line != "#~#":
-        openedfile.write(line + "\n")
 filesize = os.path.getsize(filename)
-print(filesize)
+if os.path.exists(filename):
+    print("Warning - this file already exists.")
+openedfile = open(filename, "a");
+print("Start typing. It'll automatically be saved. Type '#save' to exit.")
+while line != "#save":
+    line = input("ed > ")
+    if line == "#save":
+        pass
+    if line == "#show":
+        filer = open(filename, 'r')
+        if filesize > 0:
+            print("Previously stored:")
+            print(filer.read(), end='')
+        filer.close()
+        if len(filebuffer):
+            print("To be saved:")
+            for x in range(len(filebuffer)):
+                print(filebuffer[x])
+    if not "#" in line:
+        openedfile.write(line + "\n")
+        filebuffer.append(line)
+openedfile.close()
 print("File '" + filename + "' - " + str(filesize) + " bytes saved.")
