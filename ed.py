31bf70b0b3270e86dc8929516c8ef1df83749095
Arsalan Kazmi <sonicspeed848@gmail.com>
2021-04-15 13:57:33 +0100

    Swapped # for ~
    Python uses # for comments, so ed.py commands now use ~.

--- ed.py
+++ ed.py
@@ -15,12 +15,12 @@ filesize = os.path.getsize(filename)
 print("File '" + filename + "' opened for writing.")
 print("Start typing. Changes will be saved in buffer.")
 try:
-	while line != "#exit" and line != "#save":
+	while line != "~exit" and line != "~save":
 		line = input("ed > ")
-		if line == "#save":
+		if line == "~save":
 			for x in range(len(filebuffer)):
 				openedfile.write(filebuffer[x] + "\n")
-		elif line == "#show":
+		elif line == "~show":
 			filer = open(filename, 'r')
 			if filesize > 0:
 				print("Previously stored:")
@@ -30,20 +30,20 @@ try:
 				print("To be saved:")
 				for x in range(len(filebuffer)):
 					print(filebuffer[x])
-		elif line == "#help":
-			print("#save - Save the file and exit.")
-			print("#exit - Exit without saving.")
-			print("#show - Show the file contents, and what will be saved when the program quits.")
-		elif "#" in line:
+		elif line == "~help":
+			print("~save - Save the file and exit.")
+			print("~exit - Exit without saving.")
+			print("~show - Show the file contents, and what will be saved when the program quits.")
+		elif "~" in line:
 			print(line + " is an unknown command.")
-		if not "#" in line:
+		if not "~" in line:
 			filebuffer.append(line)
 except KeyboardInterrupt:
 	print()
 openedfile.close()
-if line == "#save":
+if line == "~save":
 	print("File '" + filename + "' - " + str(filesize) + " bytes saved.")
-elif line == "#exit":
+elif line == "~exit":
 	print("File '" + filename + "' - closed without saving.")
 else:
 	print("File '" + filename + "' - closed without saving.")
